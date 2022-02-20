4b10027dbf9fb6479af84d326d6ba6014676342b
monadabbah <monadabbah89@gmail.com>
2022-02-20 22:45:05 +0200

    Add files via upload

---
+++ storage/framework/views/1dac2287df600b4ed41c02db7936265c8361bbc9.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">
+        <router-link to="/"></router-link>
+        <router-view/>
+    </div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+   
+</body>
+</html><?php /**PATH C:\xampp\htdocs\cars_app_api\resources\views/login.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/2f475b01fcf78956cfe3b9e5e818c87faaa3ed28.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" value="<?php echo e(csrf_token()); ?>"/>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+    <title><?php echo e(__('Cars Application')); ?></title>
+</head>
+<body>
+    <!--<div id="app">
+        <router-link to="/"></router-link>
+        <router-link to="/login">login</router-link>
+        <router-link to="/register">register</router-link>
+        <router-link to="/dashboard">dashboard</router-link>
+        <router-view/>
+    </div>-->
+    <div id="app">
+       
+    </div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+   
+</body>
+</html><?php /**PATH C:\xampp\htdocs\cars_app_api\resources\views/app.blade.php ENDPATH**/ ?>
